35d5a38b1abae55d3605bf096a98c911e0c50c97
Anonymous Sheep <anon4sheep@gmail.com>
2015-03-01 02:08:10 -0500

    Added missing relogintokenrequestmessage

---
+++ BlueSheep/Common/Protocol/messages/game/approach/ReloginTokenRequestMessage.cs
@@ -0,0 +1,25 @@
+﻿// Not Generated
+using BlueSheep.Common.IO;
+using BlueSheep.Engine.Types;
+
+namespace BlueSheep.Common.Protocol.Messages
+{
+	public class ReloginTokenRequestMessage : Message
+	{
+		public new const uint ID = 6540;
+		public override uint ProtocolID
+		{
+			get { return ID; }
+		}
+			
+		public override void Serialize(BigEndianWriter writer)
+		{
+		}
+
+		public override void Deserialize(BigEndianReader reader)
+		{
+		}
+
+	}
+
+}
